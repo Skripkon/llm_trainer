0d7d8514245df51e6400ed7087bdacb0b41fa3ae
Nikolay Skripko <nskripko@icloud.com>
2025-04-08 20:34:34 +0300

    *️  Allow for import * from llm_trainer

--- llm_trainer/__init__.py
+++ llm_trainer/__init__.py
@@ -2,3 +2,11 @@ from .dataset.DataLoader import DataLoader
 from .dataset.create_dataset import create_dataset, create_dataset_from_json
 from .trainer.LLMTrainer import LLMTrainer
 from .evaluator.Evaluator import Evaluator
+
+__all__ = [
+    "DataLoader",
+    "create_dataset",
+    "create_dataset_from_json",
+    "LLMTrainer",
+    "Evaluator"
+]
